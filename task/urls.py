190cc785a5b4cb66ed4d11a470da197e939e6ea7
ahammed3216 <ayyarinavad@gmail.com>
2022-06-04 11:34:33 +0530

    login page

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home,login,signup
+from .views import home,login_view,signup
 
 urlpatterns=[
     path('',home,name="home-page"),
-    path('login/',login,name="home-page"),
+    path('login/',login_view,name="home-page"),
     path('sign_up/',signup,name="register-page")
 ]
--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from .forms import LoginForm,SignUpForm
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model,authenticate,login
+
 
 # Create your views here.
 User=get_user_model()
@@ -8,9 +9,21 @@ User=get_user_model()
 def home(request):
     return render (request,"home.html")
 
-def login(request):
+def login_view(request):
     form=LoginForm(request.POST or None)
-    return render(request,"login_page.html",{'form':form})
+    if form.is_valid():      
+        username=form.cleaned_data.get('username')
+        password=form.cleaned_data.get('password')        
+        print(form.cleaned_data)
+        user=authenticate(username=username,password=password)
+        print(user)
+        if user is not None:
+            
+            login(request,user)
+            print("sucess")
+            return redirect('/')
+
+    return render(request,"login_new.html",{'form':form})
 
 def signup(request):
     form=SignUpForm(request.POST or None)
